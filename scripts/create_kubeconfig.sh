e90163aaf237c6b936ee362bc984772423875071
Davit Torchyan <daviddt.torchyan@gmail.com>
2024-04-05 14:15:38 +0400

    changed artifact name

--- scripts/create_kubeconfig.sh
+++ scripts/create_kubeconfig.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cat <<EOF > "/tmp/test"
-$(echo "$2" | tail -n +2 | head -n -1)
+cat <<EOF > "/tmp/kube-config"
+$(echo "$1" | tail -n +2 | head -n -1)
 EOF
--- terraform/kubeconfig.tf
+++ terraform/kubeconfig.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "kubeconfig" {
   depends_on = [data.talos_cluster_kubeconfig.cp_ck]
   provisioner "local-exec" {
-    command = "sh ../scripts/create_kubeconfig.sh \"${proxmox_vm_qemu.controlplane.vmid}\" \"${yamlencode(data.talos_cluster_kubeconfig.cp_ck.kubeconfig_raw)}\""
+    command = "sh ../scripts/create_kubeconfig.sh \"${yamlencode(data.talos_cluster_kubeconfig.cp_ck.kubeconfig_raw)}\""
   }
 }
