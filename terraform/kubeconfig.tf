d44b17eb9832156f10323b390fab5e8e76de9fdd
hakobian4 <armenhakobyan781@gmail.com>
2024-04-09 18:20:33 +0400

    removednull resource from terraform

--- terraform/kubeconfig.tf
+++ terraform/kubeconfig.tf
@@ -1,4 +1,4 @@
-resource "null_resource" "kubeconfig" {
+resource "terraform_data" "kubeconfig" {
   depends_on = [data.talos_cluster_kubeconfig.cp_ck]
   provisioner "local-exec" {
     command = "sh ../scripts/create_kubeconfig.sh \"${yamlencode(data.talos_cluster_kubeconfig.cp_ck.kubeconfig_raw)}\""
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -26,10 +26,6 @@ terraform {
       source  = "hashicorp/local"
       version = "2.5.1"
     }
-    null = {
-      source  = "hashicorp/null"
-      version = "3.2.2"
-    }
   }
 
   # ##  Used for end-to-end testing on project; update to suit your needs
